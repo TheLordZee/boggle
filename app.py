8b2050ced8862cf270a93f5a5dd817a61bb85201
LordZee <l0rclz33@gmail.com>
2021-04-10 22:31:43 -0400

    Game works

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from boggle import Boggle
 from flask import *
+import json
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] ="EDFEHG^#T^3rty376gcdaw2"
@@ -8,10 +9,12 @@ boggle_game = Boggle()
 
 @app.route('/')
 def home():
+    """Shows home screen"""
     return render_template('home.html')
 
 @app.route('/game')
 def game():
+    """Initializes game by setting up high score, board, and found list"""
     if session.get('high_score'):
         high_score = session['high_score']
     else:
@@ -19,11 +22,45 @@ def game():
         high_score = 0
 
     board = boggle_game.make_board()
+    found = list()
+    session['found'] = found
     session['board'] = board
     return render_template('game.html', board=board, high_score=high_score)
     
 @app.route('/guess')
 def check_guess():
+    """Checks submitted guess and returns a boolean"""
     board = session['board']
     guess = request.args['guess']
-    return boggle_game.check_valid_word(board, guess)
+    on_board = boggle_game.check_valid_word(board, guess)
+    found = bool(guess in session['found'])
+    data = {'on_board': on_board, 'found': found}
+    return data
+
+@app.route('/found', methods=["POST"])
+def found_word():
+    """Adds guess to the found list"""
+    found = json.loads(request.data)['guess']
+    new = session['found']
+    new.append(found)
+    session['found'] = new
+    return found
+
+@app.route('/end-game')
+def end_game():
+    """Counts up the number of plays and returns the high score"""
+    score = int(request.args[' score'])
+    print('***********************************')
+    print(request.args)
+    print('***********************************')
+    if session.get('num_plays'):
+        session['num_plays'] += 1
+    else:
+        session['num_plays'] = 1
+
+    num_plays = session['num_plays']
+
+    if score > session['high_score']:
+        session['high_score'] = score
+    high_score = session['high_score']
+    return {"high_score": high_score, 'num_plays': num_plays}
--- static/game.js
+++ static/game.js
@@ -1,12 +1,86 @@
+let currGame;
+
+$('#start-game').on('click', function () {
+    currGame = new Game();
+    $('#start-game')[0].style.display = 'none'
+    $('#form')[0].style.display = 'flex'
+})
+
 
 $('#form').on('submit', async function (e) {
     e.preventDefault();
     const guess = e.target.elements[0].value;
-    const valid = await check_guess(guess)
-    console.log(valid)
+    await check_guess(guess)
+    $('#guess')[0].value = ''
 });
 
-async function check_guess(guess) {
-    const checked = await axios.get(`/guess?guess=${guess}`)
-    return checked
+$('#new-btn').on('click', function () {
+    location.reload()
+})
+
+let time = 60;
+class Game{
+    constructor(){
+        this.score =0;
+        this.countdown = setInterval(function () {
+            
+            if (time > 0) {
+                time--;
+                $('#time')[0].innerText = time
+            }
+            if (time === 0) {
+                currGame.endgame()
+            }
+        }, 1000);
+    }
+
+    async check_guess(guess) {
+        const checked = await axios.get(`/guess?guess=$ {guess}`)
+        const is_found = checked.data['found']
+        const is_checked = checked.data['on_board']
+        this.displayMessage(is_checked, is_found)
+        if ( is_found === false && is_checked === "ok") {
+            score++;
+            console.log("Found new word")
+            await axios({
+                url: 'http://127.0.0.1:5000/found',
+                method: 'POST',
+                data: { guess }
+            })
+            $('#score')[0].innerText = score
+        }
+        num = 0
+    }
+
+    displayMessage(status, found) {
+        if (found) {
+            $('#message')[0].innerText = 'Already Found Word'
+            return;
+        }
+        switch (status) {
+            case 'ok':
+                $('#message')[0].innerText = 'Found New     Word'
+                break;
+            case 'not-on-board':
+                $('#message')[0].innerText = "Word Not  On Board"
+                break;
+            case 'not-word':
+                $('#message')[0].innerText = 'Not A Word'
+                break;
+        }
+    }
+
+    async endgame() {
+        clearInterval(this.countdown)
+        console.log('GAME OVER!')
+        $('#form-btn')[0].disabled = true;
+        $('#endgame')[0].style.display = 'flex'
+        const getHigh = await axios.get(`/end-game? score=${this.score}`)
+        const highScore = getHigh.data.high_score
+        $('#end-score')[0].innerText = this.score
+        $('#end-high')[0].innerText = highScore
+        if (this.score === highScore) {
+            $('#new-high')[0].style.display = 'flex'
+        }
+    }
 }
